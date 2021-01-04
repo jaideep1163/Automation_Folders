c36d14f7644d047c7a4efe4511aa754c5330c53d
jaideep1163 <jaideep1163@gmail.com>
2021-01-04 13:57:05 +0000

    OOF

---
+++ Main.py
@@ -0,0 +1,19 @@
+import pyautogui as pg
+import time
+time.sleep(3)
+while True:
+	pg.click(pg.position(), button="right")
+	pg.hotkey('down')
+	pg.hotkey('down')
+	pg.hotkey('down')
+	pg.hotkey('down')
+	time.sleep(2)
+	pg.hotkey('enter')
+	pg.hotkey('left')
+	pg.hotkey('enter')
+	time.sleep(3)
+
+
+
+
+
